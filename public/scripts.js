029077c85d1ccabb4133f89aea85d1982784206c
Ernesto <ernesto.dpa28@gmail.com>
2026-05-11 15:00:25 -0500

    'actualizado'

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-const API_URL = 'window.location.origin';
+const API_URL = 'https://riverashop.net';
 
 let monedaActual = localStorage.getItem("monedaActual") || "COP";
 let tasaCambio = Number(localStorage.getItem("tasaCambioCache")) || 4000;
